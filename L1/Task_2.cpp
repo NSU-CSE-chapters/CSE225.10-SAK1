7964e19cfa3a5cfead200e52a430cf4f34635c50
FahimMuntashir <fahim.tech.me@gmail.com>
2021-06-29 00:36:51 +0600

    lab 01

---
+++ L1/Task_2.cpp
@@ -0,0 +1,12 @@
+//
+// Created by f12r on ২৯/৬/২১.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+}
---
+++ L1/cmake-build-debug/CMakeFiles/L3.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/L3.dir/ArrayClass.cpp.o"
+  "CMakeFiles/L3.dir/Task_1.cpp.o"
+  "CMakeFiles/L3.dir/Task_2.cpp.o"
+  "CMakeFiles/L3.dir/definition.cpp.o"
+  "CMakeFiles/L3.dir/driver.cpp.o"
+  "L3"
+  "L3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/L3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
