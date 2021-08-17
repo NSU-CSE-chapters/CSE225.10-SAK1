fd62b4cd474dfd268f3fc0b5724d3add04fef8d7
FahimMuntashir <fahim.tech.me@gmail.com>
2021-08-17 14:54:37 +0600

    added ...

--- L10/UnsortedType.h
+++ L10/UnsortedType.h
@@ -6,8 +6,38 @@
 #define L10_UNSORTEDTYPE_H
 
 
+template<class ItemType>
 class UnsortedType {
+    struct NodeType {
+        ItemType info;
+        NodeType *next;
+    };
+public:
+    UnsortedType();
 
+    ~UnsortedType();
+
+    bool IsFull();
+
+    int LengthIs();
+
+    void MakeEmpty();
+
+    void RetrieveItem(ItemType &,
+                      bool &);
+
+    void InsertItem(ItemType);
+
+    void DeleteItem(ItemType);
+
+    void ResetList();
+
+    void GetNextItem(ItemType &);
+
+private:
+    NodeType *listData;
+    int length;
+    NodeType *currentPos;
 };
 
 
